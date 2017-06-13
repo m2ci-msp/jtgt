6f5f6103aa93bf0e3221f4b8007903641e39a673
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-06-13 15:48:31 +0200

    Adding the annotation class

---
+++ src/main/java/org/m2ci/msp/jtgt/Annotation.java
@@ -0,0 +1,92 @@
+package org.m2ci.msp.jtgt;
+
+
+/**
+ * TextGrid annotation wrapper class
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
+ */
+public class Annotation
+{
+    private double _start;
+    private double _end;
+    private String _text;
+
+    /**
+     * Constructor of an annotation
+     *
+     * @param start the start of the annotation
+     * @param end the end of the annotation
+     * @param text the annotation label
+     */
+    public Annotation(double start, double end, String text)
+    {
+	setStart(start);
+	setEnd(end);
+	setText(text);
+    }
+
+    /**
+     * Get the start of the annotation
+     *
+     * @return the start of the annotation
+     */
+    public double getStart()
+    {
+	return _start;
+    }
+
+    /**
+     * Get the end of the annotation
+     *
+     * @return the end of the annotation
+     */
+    public double getEnd()
+    {
+	return _end;
+    }
+
+
+    /**
+     * Get the label of the annotation
+     *
+     * @return the label of the annotation
+     */
+    public String getText()
+    {
+	return _text;
+    }
+
+    /**
+     * Define the start of the annotation
+     *
+     * @param start the new start
+     */
+    public void setStart(double start)
+    {
+	_start = start;
+    }
+
+    /**
+     * Define the end of the annotation
+     *
+     * @param end the new end
+     */
+    public void setEnd(double end)
+    {
+	_end = end;
+    }
+
+    /**
+     * Define the label of the annotation
+     *
+     * @param label the new label
+     */
+    public void setText(String text)
+    {
+	_text = text;
+    }
+}
+
+
+/* Annotation.java ends here */
