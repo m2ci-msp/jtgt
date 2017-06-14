e1c0ec2ae74ca7dc50456fb8a0883639b728aec6
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-06-14 08:51:45 +0200

    start serializer

---
+++ src/main/java/org/m2ci/msp/jtgt/io/TextGridSerializer.java
@@ -0,0 +1,17 @@
+package org.m2ci.msp.jtgt.io;
+
+
+/**
+ *
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
+ */
+public class TextGridSerializer
+{
+
+    public TextGridSerializer()
+    {
+    }
+}
+
+/* TextGridSerializer.java ends here */
